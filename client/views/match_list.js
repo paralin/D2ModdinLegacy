0c766a97fa73a3ba780b1ef533198faccbfa25aa
Gitlab <gitlab@localhost>
2014-04-08 19:56:26 +0000

    PhantomJS SEO

--- client/views/match_list.js
+++
@@ -1,8 +0,0 @@
-Template.self.matchList.rendered = function() {
-  // things to do when the template is finished rendering
-};
-
-Template.self.matchList.events({
-  //".my-button click": function() {',
-    // do something clever',
-});
