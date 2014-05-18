903805392ed50908001bea2025ad837cc2b3581b
Christian Stewart <kidovate@gmail.com>
2014-05-18 12:03:30 -0400

    Startup script update

--- start.bash
+++ start.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 tmux start-server
-tmux new-session -d -s server -n "bash"
-tmux send-keys -t server:0 "cd /root/d2mp/; ROOT_URL=http://d2modd.in/ meteor --production --port 80" C-m
+tmux new-session -d -s server
+tmux new-window -t server:1 -n "bash"
+tmux send-keys -t server:1 "cd /root/d2mp/; ROOT_URL=http://d2modd.in/ meteor --production --port 80" C-m
